ae73457feefc18591a7eaf4a3d1abc6367c0bb73
Abdullah Kh <aboodsyabood95mnk@gmail.com>
2025-04-24 14:46:17 +0300

    تعديل جديد

--- Components/ui/CanvasRevealEffect.tsx
+++ Components/ui/CanvasRevealEffect.tsx
@@ -258,18 +258,16 @@ const ShaderMaterial = ({
   const material = useMemo(() => {
     const materialObject = new THREE.ShaderMaterial({
       vertexShader: `
-      precision mediump float;
-      in vec2 coordinates;
-      uniform vec2 u_resolution;
-      out vec2 fragCoord;
-      void main(){
-        float x = position.x;
-        float y = position.y;
-        gl_Position = vec4(x, y, 0.0, 1.0);
-        fragCoord = (position.xy + vec2(1.0)) * 0.5 * u_resolution;
-        fragCoord.y = u_resolution.y - fragCoord.y;
-      }
-      `,
+  precision mediump float;
+  uniform vec2 u_resolution;
+  out vec2 fragCoord;
+
+  void main(){
+    fragCoord = (position.xy + vec2(1.0)) * 0.5 * u_resolution;
+    fragCoord.y = u_resolution.y - fragCoord.y;
+    gl_Position = vec4(position, 1.0);
+  }
+`,
       fragmentShader: source,
       uniforms: getUniforms(),
       glslVersion: THREE.GLSL3,
